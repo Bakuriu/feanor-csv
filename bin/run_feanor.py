ab21d6c80689eb7355eeca7152cb22afb863ca5a
Giacomo Alzetta <giacomo.alzetta@gmail.com>
2018-07-15 13:19:18 +0200

    dev: can now define arbitraries and references to arbitraries from command line. Closes #4.

---
+++ bin/run_feanor.py
@@ -0,0 +1,3 @@
+from feanor import main
+
+main.main()
--- feanor/main.py
+++ feanor/main.py
@@ -1,15 +1,22 @@
-import sys
 import argparse
+import sys
 
 from feanor.schema import Schema
 from feanor.types.arbitrary import parse_type, ParsingError
 from .engine import generate_data
 
+
 def main():
     parser = argparse.ArgumentParser()
     column_typer = Column()
-    parser.add_argument('-c' ,'--column', nargs=2, help='Add a column with the given type.', type=column_typer.type,
-                        dest='columns', action='append', metavar=('NAME', 'TYPE'))
+    parser.add_argument('-c', '--column', nargs=2, type=column_typer.type,
+                        help='Add a column with the given type or using the given arbitrary.\n'
+                             'An arbitrary name must be preceded by the @ sign.',
+                        dest='columns', action='append', metavar=('NAME', 'TYPE_OR_ARBITRARY'),
+                        required=True)
+    parser.add_argument('-a', '--arbitrary', nargs=2, help='Add an arbitrary with the given type.',
+                        type=column_typer.arbitrary, dest='arbitraries', action='append', metavar=('NAME', 'TYPE'),
+                        default=[])
     parser.add_argument('--no-header', action='store_false', help='Do not add header to the output.',
                         dest='show_header')
     size_options = parser.add_mutually_exclusive_group(required=True)
@@ -21,7 +28,7 @@ def main():
                         default=sys.stdout, type=argparse.FileType('w'))
 
     args = parser.parse_args()
-    schema = make_schema(args.columns, args.show_header)
+    schema = make_schema(args.arbitraries, args.columns, args.show_header)
 
     size_dict = {}
     if args.stream_mode is not None:
@@ -34,22 +41,44 @@ def main():
     generate_data(schema, args.output_file, **size_dict)
 
 
-def make_schema(columns, show_header):
+def make_schema(arbitraries, columns, show_header):
     schema = Schema(show_header=show_header)
-    for name, (col_type, config) in columns:
-        schema.add_column(name, type=col_type, config=config)
+    for name, (arbitrary_type, config) in arbitraries:
+        schema.add_arbitrary(name, type=arbitrary_type, config=config)
+    for name, (col_type, config, arbitrary) in columns:
+        schema.add_column(name, arbitrary=arbitrary, type=col_type, config=config or None)
     return schema
 
 
 class Column:
     def __init__(self):
-        self._num_calls = 0
+        self._num_type_calls = 0
+        self._num_arbitrary_calls = 0
 
     def type(self, string):
-        """Callable that works as ArgumentParser's `type` parameter for describing the type of an arbitrary."""
-        self._num_calls += 1
-        if self._num_calls % 2 == 1:
+        """Callable that works as ArgumentParser's `type` parameter for describing the type or arbitrary of a column."""
+        self._num_type_calls += 1
+        if self._num_type_calls % 2 == 1:
+            # this is the name of the column. Keep as is.
             return string
+        # This is the "type or arbitrary name" of the column.
+        if string[:1] != '@':
+            # it's a type:
+            return self._parse_type(string) + (None,)
+        else:
+            # it's a reference to an arbitrary with that name. Remove initial @
+            return (None, None, string[1:])
+
+    def arbitrary(self, string):
+        """Callable that works as ArgumentParser's `type` parameter for describing the type of an arbitrary."""
+        self._num_arbitrary_calls += 1
+        if self._num_arbitrary_calls % 2 == 1:
+            # this is the name of the arbitrary. Keep as is.
+            return string
+        # This is the "type" of the arbitrary.
+        return self._parse_type(string)
+
+    def _parse_type(self, string):
         try:
             return parse_type(string)
         except ParsingError as e:
