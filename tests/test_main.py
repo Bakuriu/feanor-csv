938f8476c04c96aa310713ebc470598b4154a6ac
Giacomo Alzetta <giacomo.alzetta@gmail.com>
2018-07-20 19:45:17 +0200

    dev: test coverage for show_header and zero columns.

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,21 +7,21 @@ from feanor.schema import ProjectionTransformer
 
 class TestMakeSchema(unittest.TestCase):
     def test_can_make_schema_with_a_single_column(self):
-        schema = make_schema([], [('A', ('int', None, None))], True)
+        schema = make_schema([], [('A', ('int', None, None))], show_header=True)
         self.assertTrue(schema.show_header)
         self.assertEqual(('A',), schema.columns)
         self.assertEqual(1, len(schema.arbitraries))
         self.assertEqual(SimpleNamespace(name='A', type='int', config={}), schema.arbitraries[0])
 
     def test_can_make_schema_with_a_single_column_using_explicit_arbitrary(self):
-        schema = make_schema([('bob', ('int', None))], [('A', (None, None, 'bob'))], True)
+        schema = make_schema([('bob', ('int', None))], [('A', (None, None, 'bob'))], show_header=True)
         self.assertTrue(schema.show_header)
         self.assertEqual(('A',), schema.columns)
         self.assertEqual(1, len(schema.arbitraries))
         self.assertEqual(SimpleNamespace(name='bob', type='int', config={}), schema.arbitraries[0])
 
     def test_can_make_schema_with_multiple_columns(self):
-        schema = make_schema([], [('A', ('int', None, None)), ('B', ('int', None, None))], True)
+        schema = make_schema([], [('A', ('int', None, None)), ('B', ('int', None, None))], show_header=True)
         self.assertTrue(schema.show_header)
         self.assertEqual(('A', 'B'), schema.columns)
         arbitraries = sorted(schema.arbitraries, key= lambda x: x.name)
@@ -31,7 +31,7 @@ class TestMakeSchema(unittest.TestCase):
 
 
     def test_can_make_schema_with_multiple_columns_using_explicit_arbitrary(self):
-        schema = make_schema([('bob', ('int', None))], [('A', (None, None, 'bob')), ('B', ('int', None, None))], True)
+        schema = make_schema([('bob', ('int', None))], [('A', (None, None, 'bob')), ('B', ('int', None, None))], show_header=True)
         self.assertTrue(schema.show_header)
         self.assertEqual(('A', 'B'), schema.columns)
         arbitraries = sorted(schema.arbitraries, key=lambda x: x.name)
@@ -40,3 +40,16 @@ class TestMakeSchema(unittest.TestCase):
         self.assertEqual(SimpleNamespace(name='bob', type='int', config={}), arbitraries[1])
         self.assertEqual(1, len(schema.transformers))
         self.assertEqual(SimpleNamespace(name='A', inputs=['bob'], outputs=['A'], transformer=ProjectionTransformer(1, 0)), schema.transformers[0])
+
+    def test_can_make_schema_with_show_header_false(self):
+        schema = make_schema([], [('A', ('int', None, None))], show_header=False)
+        self.assertFalse(schema.show_header)
+        self.assertEqual(('A',), schema.columns)
+        self.assertEqual(1, len(schema.arbitraries))
+        self.assertEqual(SimpleNamespace(name='A', type='int', config={}), schema.arbitraries[0])
+
+    def test_can_make_schema_without_columns(self):
+        schema = make_schema([], [], show_header=False)
+        self.assertFalse(schema.show_header)
+        self.assertEqual((), schema.columns)
+        self.assertEqual(0, len(schema.arbitraries))
