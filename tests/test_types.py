e71d5b357e46179ae9216e60b3685633ddc998bc
Giacomo Alzetta <giacomo.alzetta@gmail.com>
2018-09-18 21:08:51 +0200

    dev: test coverage for types module

--- tests/test_types.py
+++ tests/test_types.py
@@ -11,6 +11,9 @@ class TestType(unittest.TestCase):
     def test_simple_types_are_different_if_different_name(self):
         self.assertNotEqual(SimpleType('float'), SimpleType('int'))
 
+    def test_simple_types_are_different_from_just_the_name(self):
+        self.assertNotEqual(SimpleType('float'), 'int')
+
     def test_parallel_types_are_equals_if_equal_subtypes(self):
         self.assertEqual(ParallelType([SimpleType('int'), SimpleType('float')]),
                          ParallelType([SimpleType('int'), SimpleType('float')]))
@@ -47,3 +50,7 @@ class TestType(unittest.TestCase):
         self.assertEqual(ChoiceType([SimpleType('int'), SimpleType('int')]), ChoiceType([SimpleType('int')]))
         self.assertEqual(hash(ChoiceType([SimpleType('int'), SimpleType('int')])),
                          hash(ChoiceType([SimpleType('int')])))
+
+    def test_choice_type_raises_error_if_different_output_numbers(self):
+        with self.assertRaises(ValueError):
+            ChoiceType([SimpleType('int'), ParallelType([SimpleType('int'), SimpleType('float')])])
