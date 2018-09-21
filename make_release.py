3e05fd48d529143ac19e216e9fca87953ec9d94c
Giacomo Alzetta <giacomo.alzetta@gmail.com>
2018-09-21 20:36:21 +0200

    ref: simplified definition of make release script

--- make_release.py
+++ make_release.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
-import argparse
 import os
 import re
 import sys
+import argparse
 import tempfile
 from itertools import chain
 
+
+VERSION_LEVELS = ('alpha', 'beta', 'rc', 'final')
+VERSION_DEFINITION_REGEX = re.compile(rb'(?P<ident>\w+)\s*=\s*(?!namedtuple)(?P<class>\w+)\((?P<args>.+)\)(?P<comment>\s*(#.*\n)?)')
 
 def get_current_version(path, verbose):
     package_init = os.path.join(path, 'feanor', '__init__.py')
@@ -14,24 +17,27 @@ def get_current_version(path, verbose):
         print('Checking package __init__.py at {!r}'.format(package_init))
     with open(package_init, 'rb') as infile:
         for line in infile:
-            normalized_line = _normalize_line(line)
-            if _is_version_definition_line(normalized_line):
-                expr = normalized_line.split(b'=')[1:]
-                if len(expr) > 1:
-                    raise ValueError('Invalid expression to define version: {!r}'.format(line))
-                arguments = re.sub(rb'^[^(]+\((.+)\)\s*$', rb'\1', expr[0]).split(b',')
-                if len(arguments) != 5:
-                    raise ValueError('Invalid number of arguments.')
-                return chain(map(int, arguments[:3]), [arguments[-2][1:-1].decode('ascii'), int(arguments[-1])])
+            match = match_version_definition_line(line)
+            if not match:
+                continue
+            arguments = match.group('args').split(b',')
+            if len(arguments) != 5:
+                raise ValueError(f'Invalid number of arguments: {len(arguments)}. Arguments detected: {arguments}')
+            cur_version = chain(map(int, arguments[:3]), [arguments[-2].strip()[1:-1].decode('ascii'), int(arguments[-1])])
+            return cur_version
+
         raise ValueError('Could not find version definition in file {!r}'.format(package_init))
 
 
-def _normalize_line(line):
-    return re.sub(rb'\s+', b'', line).lower()
+def match_version_definition_line(line):
+    return VERSION_DEFINITION_REGEX.fullmatch(line)
 
 
-def _is_version_definition_line(line):
-    return line.startswith(b'version_info=')
+def update_version_line(major, minor, bugfix, level, serial, version_match):
+    ident = version_match.group('ident')
+    comment = version_match.group('comment')
+    cls = version_match.group('class')
+    return f'{ident.decode("utf-8")} = {cls.decode("utf-8")}({major}, {minor}, {bugfix}, {repr(level)}, {serial}){comment.decode("utf-8")}'
 
 
 def write_new_version(path, major, minor, bugfix, level, serial, verbose):
@@ -43,10 +49,9 @@ def write_new_version(path, major, minor, bugfix, level, serial, verbose):
         outfile_path = os.path.join(tmpdir, 'outfile.py')
         with open(package_init, 'rb') as init_file, open(outfile_path, 'wb') as outfile:
             for line in init_file:
-                if _is_version_definition_line(_normalize_line(line)):
-                    new_line = 'version_info = VersionInfo({!r}, {!r}, {!r}, {!r}, {!r})\n'.format(
-                        major, minor, bugfix, level, serial
-                    )
+                version_match = match_version_definition_line(line)
+                if version_match:
+                    new_line = update_version_line(major, minor, bugfix, level, serial, version_match)
                     if verbose:
                         print('Changing version line from:\n\t{!r}\nto:\n\t{!r}'.format(line.decode('utf-8'), new_line))
                     outfile.write(new_line.encode('utf-8'))
@@ -60,20 +65,19 @@ def write_new_version(path, major, minor, bugfix, level, serial, verbose):
         os.rename(outfile_path, package_init)
 
 
-def main():
+def main(args=None):
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-M', '--major', action='store_true', help='Increase the major number of the version.')
     group.add_argument('-m', '--minor', action='store_true', help='Increase the minor number of the version.')
     group.add_argument('-b', '--bugfix', action='store_true', help='Increase the bugfix number of the version.')
     group.add_argument('-s', '--serial', action='store_true', help='Increase the serial number of the version.')
-    parser.add_argument('--set-level', dest='level', choices=('alpha', 'beta', 'rc', 'final'),
-                        help='Set the level of the release')
+    parser.add_argument('--set-level', dest='level', choices=VERSION_LEVELS, help='Set the level of the release')
     parser.add_argument('--package-path', default='.',
                         help='Path to the directory containing feanor package.\nDefault "." for current directory.')
     parser.add_argument('-v', '--verbose', action='store_true', help='Be verbose.')
 
-    args = parser.parse_args()
+    args = parser.parse_args(args)
     change_num = any([args.major, args.minor, args.bugfix, args.serial])
     if not any([change_num, args.level]):
         parser.print_usage(sys.stderr)
